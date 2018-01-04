ca1e4e3a9ae8d9c8e2c93fbab8171d215a831d0d
Marcelo Rocha <marc.rocha@gmail.com>
2018-01-04 18:16:38 -0400

    Updating MovieAdapter with glide

--- app/src/main/java/com/challenge/ndrive/tmdbexplorer/adapter/MovieAdapter.java
+++ app/src/main/java/com/challenge/ndrive/tmdbexplorer/adapter/MovieAdapter.java
@@ -1,13 +1,16 @@
 package com.challenge.ndrive.tmdbexplorer.adapter;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.challenge.ndrive.tmdbexplorer.R;
 import com.challenge.ndrive.tmdbexplorer.model.Movie;
 
@@ -18,6 +21,8 @@ import java.util.ArrayList;
  */
 
 public class MovieAdapter extends ArrayAdapter<Movie> {
+
+    final String BASE_URL = "https://image.tmdb.org/t/p/w92";
 
     public MovieAdapter(@NonNull Context context, ArrayList<Movie> movies) {
         super(context, 0, movies);
@@ -32,6 +37,20 @@ public class MovieAdapter extends ArrayAdapter<Movie> {
         }
 
         Movie currentMovie = getItem(position);
+
+        Uri builder = Uri.parse(BASE_URL)
+                .buildUpon()
+                .appendPath(currentMovie.getPoster​Image())
+                .build();
+
+        String posterImage = builder.toString();
+
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.list_item_image);
+
+        Glide.with(getContext())
+                .load(posterImage)
+                .error(R.drawable.image_error)
+                .into(imageView);
 
 
 
