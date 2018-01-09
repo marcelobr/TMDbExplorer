cb62a995e787f57f84b3a9168772e5612ad5d078
Marcelo Rocha <marc.rocha@gmail.com>
2018-01-09 19:32:16 -0400

    Adding improvements and unit test

--- app/src/main/java/com/challenge/ndrive/tmdbexplorer/adapter/MovieAdapter.java
+++ app/src/main/java/com/challenge/ndrive/tmdbexplorer/adapter/MovieAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.net.Uri;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,22 +17,14 @@ import com.challenge.ndrive.tmdbexplorer.interfaces.TmdbClient;
 import com.challenge.ndrive.tmdbexplorer.model.Movie;
 import com.challenge.ndrive.tmdbexplorer.utils.TmdbImageType;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 /**
  * Created by marcelo on 04/01/18.
  */
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
-
-    /** Tag for the log messages */
-    private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
 
     private List<Movie> mMoviesList = null;
     private Context mContext;
@@ -94,19 +85,11 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         holder.mTitleTextView.setText(currentMovie.getTitle());
 
         // Create a new Date object from the time of the web publication date
-        Date dateObject;
-        try {
-            DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-            dateObject = df.parse(currentMovie.getReleaseDate());
-            Calendar movieDate = Calendar.getInstance();
-            movieDate.setTime(dateObject);
-            int movieYear = movieDate.get(Calendar.YEAR);
+        int movieYear = currentMovie.getYear();
+        if (movieYear != 0) {
             holder.mYearTextView.setText(String.valueOf(movieYear));
-            Calendar currentDate = Calendar.getInstance();
-            int currentYear = currentDate.get(Calendar.YEAR);
-            holder.mYearTextView.setTextColor(movieYear == currentYear ? colorRed : colorDefault);
-        } catch(ParseException e) {
-            Log.e(LOG_TAG, "Date parse error ", e);
+            holder.mYearTextView.setTextColor(movieYear == getCurrentYear() ? colorRed : colorDefault);
+        } else {
             holder.mYearTextView.setText(R.string.date_error_message);
         }
     }
@@ -131,5 +114,10 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             this.mMoviesList.clear();
             notifyDataSetChanged();
         }
+    }
+
+    private int getCurrentYear() {
+        Calendar currentDate = Calendar.getInstance();
+        return currentDate.get(Calendar.YEAR);
     }
 }
--- app/src/main/java/com/challenge/ndrive/tmdbexplorer/model/Movie.java
+++ app/src/main/java/com/challenge/ndrive/tmdbexplorer/model/Movie.java
@@ -1,10 +1,23 @@
 package com.challenge.ndrive.tmdbexplorer.model;
 
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Created by marcelo on 03/01/18.
  */
 
 public class Movie {
+
+    /** Tag for the log messages */
+    private static final String LOG_TAG = Movie.class.getSimpleName();
 
     private long mId;
     private String mTitle;
@@ -43,10 +56,6 @@ public class Movie {
         return mTitle;
     }
 
-    public String getReleaseDate() {
-        return mReleaseDate;
-    }
-
     public String getPoster​Image() {
         return mPoster​Image;
     }
@@ -73,5 +82,37 @@ public class Movie {
 
     public int getRuntime() {
         return mRuntime;
+    }
+
+    @Nullable
+    public Date getReleaseDate() {
+        Date date = null;
+
+        if (this.mReleaseDate != null) {
+            try {
+                DateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                date = df.parse(this.mReleaseDate);
+            } catch (ParseException e) {
+                Log.e(LOG_TAG, "Unknown date format", e);
+            }
+        }
+
+        return date;
+    }
+
+    /**
+     * @return The year of release date or 0 when unknown.
+     */
+    public int getYear() {
+        Date date = getReleaseDate();
+
+        if (date == null) {
+            return 0;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        return calendar.get(Calendar.YEAR);
     }
 }
